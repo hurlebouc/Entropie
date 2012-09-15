f117a55b807120a639ad0fafb8f5787440c171d8
hurlebouc <mathsmac@yahoo.fr>
2012-09-15 10:16:06 +0200

    Initial Commit

---
+++ Entropie/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Entropie
+//
+//  Created by Hubert on 15/09/12.
+//  Copyright (c) 2012 Hubert. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
